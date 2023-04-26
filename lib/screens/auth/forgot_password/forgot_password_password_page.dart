2fef5e78756f4623ff884ffa86988a4550005cce
unknown <you@example.com>
2023-04-26 20:50:53 +0200

    added enter new password

--- lib/screens/auth/forgot_password/forgot_password_password_page.dart
+++ lib/screens/auth/forgot_password/forgot_password_password_page.dart
@@ -1,4 +1,7 @@
+import 'package:chattingapp/constants.dart';
+import 'package:chattingapp/widgets/Input_Textfield_widget.dart';
 import 'package:chattingapp/widgets/app_bar_widget.dart';
+import 'package:chattingapp/widgets/button_widget.dart';
 import 'package:flutter/material.dart';
 
 class EnterNewPassword extends StatefulWidget {
@@ -9,14 +12,65 @@ class EnterNewPassword extends StatefulWidget {
 }
 
 class _EnterNewPasswordState extends State<EnterNewPassword> {
+  final newpasswordController = TextEditingController();
+  final confirmpasswordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: AppBarWidget(title: 'New Password', icon: Icons.arrow_back),
-      body: Center(
-        child: Text('Password Reset Screen', style: TextStyle(fontSize: 40)),
-      )
+    return Scaffold(
+      appBar: const AppBarWidget(title: 'New Password', icon: Icons.arrow_back),
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const[
+                     Text(
+                      'Create your new password', 
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 20),
+                // Enter new password textfield
+                InputFields(
+                  controller: newpasswordController,
+                  icon:Icons.lock_rounded,
+                  hintText: 'Password',
+                  obscureText: true,
+                ),
+
+                const SizedBox(height: 30),
+                
+                // confirm new password textfield
+                InputFields(
+                  controller: confirmpasswordController,
+                  icon:Icons.lock_rounded,
+                  hintText: 'Password',
+                  obscureText: true,
+                ),
+
+                //Forgot Password button
+              
+                const SizedBox(height: 100),
+              
+                ButtonWidget(
+                  text: "Continue",
+                  press: () { 
+                    Navigator.pushNamed(context, homepage);
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
